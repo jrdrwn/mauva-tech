0c669099837992c460987c0256b613ced29dd413
Jordi Irawan <jordirwn@gmail.com>
2025-02-18 10:57:51 +0700

    chore: change background color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ import { ModeToggle } from './ui/ModeToggle'
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-white dark:border-gray-800 dark:bg-gray-950">
+    <header className="sticky top-0 z-50 w-full border-b bg-background border-border">
       <div className="container mx-auto flex h-16 max-w-6xl items-center justify-between px-4 md:px-6">
         <Link href="#" className="flex items-center gap-2" prefetch={false}>
           <MountainIcon className="h-6 w-6" />
