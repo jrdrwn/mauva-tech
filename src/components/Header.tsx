4dfdd342711e3648e06c356bc4a528dbb5dff43e
itsmerahmat <rahmatgamming1@gmail.com>
2025-02-18 22:18:32 +0800

    feat: add contact form and input components with motion effects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
           <Link href="#" className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" prefetch={false}>
             Services
           </Link>
-          <Link href="#" className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" prefetch={false}>
+          <Link href="/contact" className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" prefetch={false}>
             Contact
           </Link>
         </nav>
