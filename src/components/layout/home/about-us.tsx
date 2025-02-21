303833d3c86ad8c175b2c334a75e73ce8bf11996
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-02-21 23:43:47 +0700

    fix: disable exhaustive-deps rule for useEffect in AboutUs component to prevent linting errors

--- src/components/layout/home/about-us.tsx
+++ src/components/layout/home/about-us.tsx
@@ -134,6 +134,7 @@ export const Grid = ({
         [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
       ]
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
