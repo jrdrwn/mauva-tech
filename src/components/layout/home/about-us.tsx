dd0a7997e1efbb3fdb4958f349af6e733dda61ef
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-02-21 23:37:30 +0700

    refactor: update pattern handling in AboutUs component to use state and effect for better reactivity

--- src/components/layout/home/about-us.tsx
+++ src/components/layout/home/about-us.tsx
@@ -12,7 +12,7 @@ import {
 import { WobbleCard } from "@/components/ui/wobble-card";
 import { MoveUpRight } from "lucide-react";
 import Image from "next/image";
-import { useId } from "react";
+import { useEffect, useId, useState } from "react";
 
 export default function AboutUs() {
   return (
@@ -123,13 +123,19 @@ export const Grid = ({
   pattern?: number[][];
   size?: number;
 }) => {
-  const p = pattern ?? [
-    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-    [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
-  ];
+  const [p, setP] = useState<number[][]>(pattern ?? []);
+  useEffect(() => {
+    setP(
+      pattern ?? [
+        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+        [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
+      ]
+    );
+  }, []);
+
   return (
     <div className="pointer-events-none absolute  top-0   h-full w-full [mask-image:linear-gradient(white,transparent)]">
       <div className="absolute inset-0 bg-gradient-to-r  [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-zinc-900/30 from-zinc-100/30 to-zinc-300/30 dark:to-zinc-900/30 opacity-100">
