1dca7fae5766cd15d8123cd4105b821a2bfe31a0
Jordi Irawan <jordirwn@gmail.com>
2025-02-27 12:27:42 +0700

    feat: add About page and related components for improved site navigation [draft]

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Hero from '@/components/layout/about/hero';
+import EndCTA from '@/components/layout/shared/end-cta';
+import Footer from '@/components/layout/shared/footer';
+import Header from '@/components/layout/shared/header';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <EndCTA />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/layout/about/hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="container mx-auto flex flex-col items-center justify-center py-20">
+        <h1 className="mb-2 text-center text-4xl font-medium">About Us</h1>
+        <p className="text-center text-lg text-secondary-foreground/80">
+          We are a team of passionate developers, designers, and marketers who
+          are dedicated to helping our clients achieve their goals.
+        </p>
+      </div>
+    </section>
+  );
+}
